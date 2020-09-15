a00faf7d070f19c7b31a23ee362555d423d8ef68
Maximilian Coghlan <max.coghlan@icloud.com>
2020-09-15 17:35:02 +0100

    component structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const Editor = () => {
+    return (
+      <div className="editor-section">
+        <div className="title-bar">
+          <i className="fas fa-code"></i>
+          <p>Editor</p>
+          <i className="fas fa-arrows-alt"></i>
+        </div>
+        <textarea name="editor" id="editor" cols="30" rows="10"></textarea>
+      </div>
+    );
+  };
+
+  const Preview = () => {
+    return (
+      <div className="preview-section">
+        <div className="title-bar">
+          <i className="fas fa-code"></i>
+          <p>Preview</p>
+          <i className="fas fa-arrows-alt"></i>
+        </div>
+        <div id="preview"></div>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      <h1>Hello bitch</h1>
-    </div>
+    <>
+      <Editor />
+      <Preview />
+    </>
   );
 }
 
